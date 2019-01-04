0ca03b692ea8b4b2c9d26bd093b74a13445c8835
Jean Carlos Antunes da Crus <42682200+jeancrus@users.noreply.github.com>
2019-01-04 16:53:00 -0200

    Update Exemplo.java

--- ProjetoTesteGitEclipse/src/application/Exemplo.java
+++ ProjetoTesteGitEclipse/src/application/Exemplo.java
@@ -5,6 +5,7 @@ public class Exemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Teste java eclipse git");
+		System.out.println("Alteracao 1");
 	}
 
 }
