e75502ba902d3549c9bd801e6478d8a1be18c316
Jean Crus <jean.carlos@pucpr.edu.br>
2019-01-04 16:55:02 -0200

    Modificado eclipse

--- ProjetoTesteGitEclipse/src/application/Exemplo.java
+++ ProjetoTesteGitEclipse/src/application/Exemplo.java
@@ -5,7 +5,6 @@ public class Exemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Teste java eclipse git");
-		System.out.println("Alteracao 1");
 	}
 
 }
